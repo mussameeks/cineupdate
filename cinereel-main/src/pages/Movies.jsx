40b2cc15ad37c0ef15a701934c00e573b0a60d3b
mussameeks <mussameeks@gmail.com>
2025-08-06 22:35:46 +0200

    Delete cinereel-main directory

--- cinereel-main/src/pages/Movies.jsx
+++
@@ -1,3 +0,0 @@
-export default function Movies() {
-  return <div className="p-4 text-white">Browse Movies</div>;
-}
--- cinereel-main/src/pages/Trailers.jsx
+++
@@ -1,3 +0,0 @@
-export default function Trailers() {
-  return <div className="p-4 text-white">Explore Trailers</div>;
-}
